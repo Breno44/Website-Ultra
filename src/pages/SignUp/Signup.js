b299006d1852d6fbd2afcb36d746f086d75d3a5b
Breno Henrique <bhfs.2908@gmail.com>
2021-09-03 09:07:43 -0300

    feat:finish website

---
+++ src/pages/SignUp/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InfoSection from "../../components/InfoSection/index";
+import { homeObjFour } from "./Data";
+
+const Signup = () => {
+  return (
+    <>
+      <InfoSection {...homeObjFour} />
+    </>
+  );
+};
+
+export default Signup;
